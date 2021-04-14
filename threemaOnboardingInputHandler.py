5f1e5b2a7d63c1f91cc34c33323595763e5952ab
mmuellerstoffels <marc@denamics.com>
2021-04-14 20:40:09 +0200

    Working version. Used for onboarding FB 9 and 10. A little rough around the edges.

--- threemaOnboardingInputHandler.py
+++ threemaOnboardingInputHandler.py
@@ -1,25 +1,112 @@
+#  Copyright (c) 2021 by Marc Mueller-Stoffels (https://github.com/mmuellerstoffels/) is licensed under CC BY-SA 4.0.
+#  To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/4.0/
+
 import pandas as pd
-from appliances import stringFlattener, passwordGenerator
+from appliances import stringFlattener, passwordGenerator, fileChooser, duplicateWarningDialogShell, mainView
 
+# Fire up the main view
+root = mainView()
 
-inptDF = pd.read_excel('/Users/marc/PycharmProjects/pythonProject/ThreemaOnboarding/input/Kopie von Threema Bedarfe mit E-Mail-Adressen_.xlsx')
+# Load the file with the users that will be imported. This is an Excel file with at least the columns 'Firstname' and
+# 'LastName'.
+inputFileName = fileChooser(root, 'Select the XLSX file with new users.', [("XLSX Files", "*.xlsx")])
+inptDF = pd.read_excel(inputFileName)
+inptDF.dropna(how='all', inplace=True)
 
+# Add columns to the input dataframe for the generated username and password
 inptDF["Username"] = ""
 inptDF["Password"] = ""
 
+# Retrieve the current MDM file for checking new usernames against as duplicates ares not allowed.
+mdmFileName = fileChooser(root, 'Select the CSV file containing the MDM infos.', [('CSV Files', '*.csv')])
+mdmDf = pd.read_csv(mdmFileName)
+
+# TODO handle duplicates in input file
+print(inptDF.shape)
+inptDF.drop_duplicates(subset=['FirstName', 'LastName'], keep='first', inplace=True, ignore_index=True)
+print(inptDF.shape)
 
 for i in inptDF.index:
-    nameFlattened = stringFlattener(inptDF.Nachname[i])
+    nameFlattened = stringFlattener(inptDF.LastName[i])
     try: #handle empty first names
-        fnameInit = stringFlattener(inptDF.Vorname[i][:2])
+        fnameInit = stringFlattener(inptDF.FirstName[i])[:2]
     except:
         fnameInit = 'n'
     userName = fnameInit + nameFlattened
-    userName = userName.lower()
-    inptDF["Username"][i] = userName
-    inptDF["Password"][i] = passwordGenerator()
+    userName = userName.lower().replace("-","").replace("'","").strip().replace(" ","")
+
+    # TODO Check if userName already exists in list of new users
+    skipped = False
+    if inptDF["Username"].str.contains(userName).any():
+        # TODO Retrieve the duplicate info from the inptDF
+        idx = inptDF["Username"].str.contains(userName).idxmax()
+
+        validAction = False
+        while not validAction:
+            action = duplicateWarningDialogShell('Import Data', userName,
+                                                 inptDF.FirstName[idx], inptDF.LastName[idx],
+                                                 inptDF.FirstName[i], inptDF.LastName[i])
+
+            if action == 's':
+                inptDF.drop([i], inplace=True)
+                print("User creation skipped")
+                validAction = True
+                skipped = True
+                # No further action here
+            elif action == 'm':
+                appendInt = 1
+                userNameMod = userName
+                while inptDF['Username'].str.contains(userNameMod).any():
+                    userNameMod = userName + str(appendInt)
+                userName = userNameMod
+                print('Username modified to ' + userNameMod)
+                validAction = True
+            else:
+                print('########### WARNING #############')
+                print('Invalid action. Please try again.')
+
+    # add username and password
+    print(i)
+    if not skipped:
+        inptDF.loc[i, 'Username'] = userName
+        inptDF.loc[i, 'Password'] = passwordGenerator()
+        skipped = False
 
 
+for i in inptDF.index:
+    userName = inptDF.loc[i, 'Username']
+    # check if username exists already in current MDM
+    for j in mdmDf.index[1:]:
+        if userName == mdmDf.license[j]:
+            validAction = False
+            while not validAction:
+                action = duplicateWarningDialogShell('MDM', userName,
+                                            mdmDf.th_firstname[j], mdmDf.th_lastname[j],
+                                            inptDF.FirstName[i], inptDF.LastName[i])
+                print('i: ' + str(i) + ', j: ' + str(j))
+                if action == 's':
+                    inptDF.drop([i], inplace=True)
+                    print("User creation skipped")
+                    validAction = True
+                    #No further action here
+                elif action == 'm':
+                    appendInt = 1
+                    userNameMod = userName
+                    while mdmDf['license'].str.contains(userNameMod).any():
+                        userNameMod = userName + str(appendInt)
+                    userName = userNameMod
+                    inptDF.loc[i, 'Username'] = userNameMod
+                    print('Username modified to ' + userNameMod)
+                    validAction = True
+                else:
+                    print('########### WARNING #############')
+                    print('Invalid action. Please try again.')
+
+
+
+
+
+print(inptDF)
 
 writer = pd.ExcelWriter('../../output/output.xlsx')
 inptDF.to_excel(writer)
--- threemaOnboardingMDMHandler.py
+++ threemaOnboardingMDMHandler.py
@@ -1,14 +1,16 @@
 import pandas as pd
 
-mdmDF = pd.read_csv('input/threema-mdm-export-2021-03-31.csv')
-userListDF = pd.read_excel('input/output_Personalrat_20210331.xlsx')
+mdmDF = pd.read_csv('input/threema-mdm-export-2021-04-14.csv')
+userListDF = pd.read_excel('output/output.xlsx')
 
 for i in mdmDF.index[1:]:
-    if userListDF['Username'].str.contains(mdmDF.license[i]).any():
-        fname = userListDF.loc[userListDF['Username'] ==  mdmDF.license[i], 'Vorname'].values[0].strip()
-        lname = userListDF.loc[userListDF['Username'] ==  mdmDF.license[i], 'Nachname'].values[0].strip()
+    if userListDF['Username'].str.fullmatch(mdmDF.license[i]).any():
+        print(mdmDF.license[i])
+        fname = userListDF.loc[userListDF['Username'] == mdmDF.license[i], 'FirstName'].values[0].strip()
+        lname = userListDF.loc[userListDF['Username'] == mdmDF.license[i], 'LastName'].values[0].strip()
         mdmDF.th_nickname[i] = fname + ' ' + lname
         mdmDF.th_firstname[i] = fname
         mdmDF.th_lastname[i] = lname
 
+print(mdmDF)
 mdmDF.to_csv('output/mdmOutput.csv', index = False)
