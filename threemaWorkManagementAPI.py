41562bd41780f3759ab59a28c0784dffac62c95a
mmuellerstoffels <marc@denamics.com>
2021-04-24 09:10:07 +0200

    Threema Work Management Cockpit API shell all functions.

--- threemaWorkManagementAPI.py
+++ threemaWorkManagementAPI.py
@@ -41,4 +41,94 @@ class ThreemaWorkManagementAPI:
     def getListUsers(self, filterCredential, filterUsername, filterQuery, pageSize='20', page='0'):
         pass
 
+    # TODO
+    def getShowUser(self, id):
+        pass
+
+    # TODO
+    def deleteRevokeUser(self, id):
+        pass
+
+    # TODO
+    def postUnlinkUser(self, id):
+        pass
+
+    #### CONTACTS ########
+    # TODO
+    def getShowContact(self, id):
+        pass
+
+    # TODO
+    def putUpdateContact(self, id):
+        pass
+
+    # TODO
+    def deleteRemoveContact(self, id):
+        pass
+
+    # TODO
+    def getShowContacts(self, filterType, pageSize='20', page='0'):
+        pass
+
+    # TODO
+    def postCreateContact(self):
+        pass
+
+    ##### THREEMA MDM #######
+    # TODO
+    def getTMDMCredentialShow(self, id, propertyId):
+        pass
+
+    # TODO
+    def putTMDMCredentialUpdate(self, id, propertyId):
+        pass
+
+    # TODO
+    def deleteTMDMGlobalRemove(self, id, propertyId):
+        pass
+
+    # TODO
+    def getTMDMCredentialList(self, id):
+        pass
+
+    # TODO
+    def postTMDMCredentialCreate(self, id):
+        pass
+
+    # TODO
+    def getTMDMGlobalShow(self, propertyId):
+        pass
+
+    # TODO
+    def putTMDMGlobalUpdate(self, propertyId):
+        pass
+
+    # TODO
+    def deleteTMDMGlobalRemove(self, propertyId):
+        pass
+
+    # TODO
+    def getTMDMGlobalList(self):
+        pass
+
+    # TODO
+    def postTMDMGlobalCreate(self, mdmValue):
+        pass
+
+    ##### LOGOS ######
+
+    def getShowLogos(self):
+        '''
+        Returns the location of the in-app logos.
+
+        :return: JSON
+        '''
+        endpoint = self.url + "/logos"
+        response = requests.get(endpoint, headers=self.headers)
+        return response
+
+    # TODO
+    def putUpdateLogos(self, logosUpdate):
+        pass
+
 
